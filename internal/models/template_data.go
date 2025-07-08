f92834d3459062f9d1c12521e2f975fd4c7cc06f
koki-develop <kou.pg.0131@gmail.com>
2025-07-08 21:47:30 +0900

    refactor: remove old layered architecture
    - Remove internal/models/, internal/providers/, internal/services/
    - Remove individual command files from cmd/
    - Remove mockery configuration and generated mocks
    - Update dependencies in go.mod (upgrade go-github v50 to v66)
    - Simplify dependency tree

--- internal/models/template_data.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// TemplateData contains data available for template processing in configuration files
-type TemplateData struct {
-	// Version is the current git tag version (e.g., "v1.0.0")
-	Version string
-	// Module is the Go module name from go.mod
-	Module string
-}
